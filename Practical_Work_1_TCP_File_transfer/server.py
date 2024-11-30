72324e2b507b9d128132428986dcb1407fb71a65
TranDucHuyy <huytd.code21@gmail.com>
2024-11-30 12:47:49 +0700

    Complete lab 1

--- Practical_Work_1_TCP_File_transfer/server.py
+++ Practical_Work_1_TCP_File_transfer/server.py
@@ -1,44 +1,52 @@
 import socket
 
 def receive_file():
-    server_ip = '127.0.0.1'
+    server_ip = '0.0.0.0'  
     server_port = 21003
+    allowed_ips = ['127.0.0.1', '192.168.48.140', '172.30.176.1'] 
 
     try:
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_socket.bind((server_ip, server_port))
-        server_socket.listen(1)
+        server_socket.listen(5) 
 
         print(f"Server is waiting for connection at {server_ip}:{server_port}...")
 
-        client_socket, client_address = server_socket.accept()
-        print(f"Connected to {client_address}")
+        while True: 
+            client_socket, client_address = server_socket.accept()
+            print(f"Connected to {client_address}")
 
-        file_info = client_socket.recv(1024).decode()
-        filename, file_size = file_info.split(',')
-        file_size = int(file_size)
+            if client_address[0] not in allowed_ips:
+                print(f"Connection from {client_address[0]} is not allowed.")
+                client_socket.close()
+                continue 
 
-        print(f"Receiving file: {filename} (Size: {file_size} bytes)")
+            file_info = client_socket.recv(1024).decode()
+            filename, file_size = file_info.split(',')
+            file_size = int(file_size)
 
-        client_socket.send("Ready to receive file".encode())
+            print(f"Receiving file: {filename} (Size: {file_size} bytes)")
 
-        with open(f'received_{filename}', 'wb') as file:
-            bytes_received = 0
-            while bytes_received < file_size:
-                data = client_socket.recv(1024)
-                file.write(data)
-                bytes_received += len(data)
+            client_socket.send("Ready to receive file".encode())
 
-                client_socket.send("ACK".encode())
+            with open(f'received_{filename}', 'wb') as file:
+                bytes_received = 0
+                while bytes_received < file_size:
+                    data = client_socket.recv(1024)
+                    file.write(data)
+                    bytes_received += len(data)
 
-        print(f"File '{filename}' received successfully and saved.")
+                    client_socket.send("ACK".encode())
+
+            print(f"File '{filename}' received successfully and saved.")
+
+            client_socket.close()
 
     except Exception as e:
         print(f"Error: {e}")
 
     finally:
-        client_socket.close()
-        server_socket.close()
+        server_socket.close() 
 
 if __name__ == "__main__":
     receive_file()
